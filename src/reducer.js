859ce041c54d5b531a6de8fbaf9a2d4ebbec6b15
Muci <mucahidcanlar@gmail.com>
2019-04-06 21:23:45 +0200

    look i dont know what i changed but my pizza is here and im calling it a day

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,9 @@
-const reducer = (state = [], action = {}) => {
+import data from './App'
+
+console.log(data)
+const initialstate = data
+
+const reducer = (state = initialstate, action = {}) => {
   switch (action.type){
     case 'ADD_MODEL':
     return [...state, action.payload]
